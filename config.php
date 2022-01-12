696a85afc7d0d508d3d51ea09cb140673c2e926d
vmbdev <11931292+vmbdev@users.noreply.github.com>
2022-01-12 16:13:21 +0100

    Fixing indentation

--- config.php
+++ config.php
@@ -8,17 +8,17 @@ class Config {
 	public static $salt = '23rifjejg29028ggj2i45r';
 
 	public static $languages = array(
-                                        'en_GB' => array('flag' => 'gb', 'name' => 'English'),
-                                        'cs_CZ' => array('flag' => 'cz', 'name' => 'Czech'),
-										'de_DE' => array('flag' => 'de', 'name' => 'German'),
-                                        'el_GR' => array('flag' => 'gr', 'name' => 'Greek'),
-										'it_IT' => array('flag' => 'it', 'name' => 'Italian'),
-										'pt_PT' => array('flag' => 'pt', 'name' => 'Portuguese'),
-                                        'es_ES' => array('flag' => 'es', 'name' => 'Spanish')
-                                    );
+					'en_GB' => array('flag' => 'gb', 'name' => 'English'),
+					'cs_CZ' => array('flag' => 'cz', 'name' => 'Czech'),
+					'de_DE' => array('flag' => 'de', 'name' => 'German'),
+					'el_GR' => array('flag' => 'gr', 'name' => 'Greek'),
+					'it_IT' => array('flag' => 'it', 'name' => 'Italian'),
+					'pt_PT' => array('flag' => 'pt', 'name' => 'Portuguese'),
+					'es_ES' => array('flag' => 'es', 'name' => 'Spanish')
+					);
 
 	public static $defaultLanguage = 'en_GB';
-    public static $defaultModule = 'home';
+	public static $defaultModule = 'home';
 
 	public static $wwwroot = '';
 	public static $dirroot = 'c://svipul';
